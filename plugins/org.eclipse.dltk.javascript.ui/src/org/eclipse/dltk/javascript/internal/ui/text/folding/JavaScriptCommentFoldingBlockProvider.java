87b0a5bcb98d01b9d4ba023151025373270d0d4a
Johan Compagner <jcompagner@gmail.com>
2013-09-25 15:47:27 +0200

    header support in js files, if first typed partition is a comment partition and it doesn't start right away with "var" or "function" it will be seen as header that needs to be collapsed

--- plugins/org.eclipse.dltk.javascript.ui/src/org/eclipse/dltk/javascript/internal/ui/text/folding/JavaScriptCommentFoldingBlockProvider.java
+++ plugins/org.eclipse.dltk.javascript.ui/src/org/eclipse/dltk/javascript/internal/ui/text/folding/JavaScriptCommentFoldingBlockProvider.java
@@ -15,6 +15,12 @@ import org.eclipse.dltk.javascript.internal.ui.text.JSPartitioningProvider;
 import org.eclipse.dltk.javascript.ui.text.IJavaScriptPartitions;
 import org.eclipse.dltk.ui.text.folding.IFoldingContent;
 import org.eclipse.dltk.ui.text.folding.PartitioningFoldingBlockProvider;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.Document;
+import org.eclipse.jface.text.IDocumentPartitioner;
+import org.eclipse.jface.text.ITypedRegion;
+import org.eclipse.jface.text.TextUtilities;
+import org.eclipse.jface.text.rules.FastPartitioner;
 
 public class JavaScriptCommentFoldingBlockProvider extends
 		PartitioningFoldingBlockProvider {
@@ -31,6 +37,65 @@ public class JavaScriptCommentFoldingBlockProvider extends
 			computeBlocksForPartitionType(content,
 					IJavaScriptPartitions.JS_MULTI_LINE_COMMENT,
 					JavaScriptFoldingBlockKind.COMMENT, isCollapseComments());
+		}
+		if (isCollapseHeaderComment()) {
+			final String contents = content.getSourceContents();
+			if (contents != null && contents.length() >= 2) {
+				Document document = new Document(contents);
+				final IDocumentPartitioner partitioner = new FastPartitioner(
+						JSPartitioningProvider.getInstance()
+								.createPartitionScanner(),
+						JSPartitioningProvider.getInstance()
+								.getPartitionContentTypes());
+				partitioner.connect(document);
+				document.setDocumentPartitioner(JSPartitioningProvider
+						.getInstance().getPartitioning(), partitioner);
+				try {
+					ITypedRegion region = TextUtilities.getPartition(document,
+							JSPartitioningProvider.getInstance()
+									.getPartitioning(), 1, true);
+					if (region.getType() == IJavaScriptPartitions.JS_MULTI_LINE_COMMENT) {
+						int endPos = region.getOffset() + region.getLength();
+						String txtAfterComment = document.get(endPos,
+								Math.min(20, document.getLength() - endPos));
+						int lineDelimiterCount = 0;
+						for (int i = 0; i < txtAfterComment.length(); i++) {
+							char c = txtAfterComment.charAt(i);
+							if (c == 'f') {
+								if (txtAfterComment.substring(i).startsWith(
+										"function"))
+									break;
+							} else if (c == 'v') {
+								if (txtAfterComment.substring(i).startsWith(
+										"var"))
+									break;
+							} else if (lineDelimiterCount == 1) {
+								// if after 1 line delimiter still no function
+								// or var is found that it is fine.
+								requestor.acceptBlock(region.getOffset(),
+										endPos,
+										JavaScriptFoldingBlockKind.COMMENT,
+										null, true);
+							} else if (c == '\n') {
+								lineDelimiterCount++;
+							} else if (c == '\r') {
+								lineDelimiterCount++;
+								if (i + 1 < txtAfterComment.length()
+										&& txtAfterComment.charAt(i + 1) == '\n')
+									i++; // skip \r\n
+							}
+							if (lineDelimiterCount == 2) {
+								requestor.acceptBlock(region.getOffset(),
+										endPos,
+										JavaScriptFoldingBlockKind.COMMENT,
+										null, true);
+								break;
+							}
+						}
+					}
+				} catch (BadLocationException e) {
+				}
+			}
 		}
 		if (isFoldingDocs())
 			computeBlocksForPartitionType(content,
